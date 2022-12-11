1704f2ea7f9282ed1239fb25818d0debc805cd96
Srinanda Yallpragada <srinanda.yallapragada@gmail.com>
2022-12-11 18:59:29 -0500

    code in progress

---
+++ execute_moves/execute_moves.ino
@@ -0,0 +1,41 @@
+
+using namespace std;
+
+
+int execute_moves(String moves_to_exec[]) {
+  // put your main code here, to run repeatedly:
+    Serial.println(moves_to_exec[0]);
+  //read each move (tokenized by space only) into an array
+  //for each input in the array, (there are a total of 3 * 6 18 possible values in the array to check)
+  //check case, and execute move
+  //return at the end of array as cube is solved. 
+
+  delay(500);
+  return 0;
+}
+
+
+
+
+void setup() {  
+  delay(500);
+  Serial.begin(9600);
+
+  //listen to laptop to fill this variable's information out.
+  char * string_moves_to_exec = "D2 R' D' F2 B D R2 D2 R' F2 D' F2 U' B2 L2 U2 D R2 U";
+  char *token;
+
+   token = strtok(string_moves_to_exec, ' ');
+
+   while (token != NULL) {
+      Serial.println(token);
+      token=strtok(NULL, ' ');
+   }
+}
+
+
+
+
+void loop(){
+
+}
