59e874e22357dd2ff99a328573e8df61861b0a94
Srinanda Yallpragada <srinanda.yallapragada@gmail.com>
2022-12-11 17:05:52 -0500

    pushing new stepper motor code

--- basic_stpper_working/basic_stpper_working.ino
+++ basic_stpper_working/basic_stpper_working.ino
@@ -1,22 +1,70 @@
-#include <Stepper.h>
+#define A 2
+#define B 3
+#define C 4
+#define D 5
+#define E 6
+#define F 7
+#define G 8
+#define H 9
+#define I 10
+#define J 11
+#define K 12
+#define NUMBER_OF_STEPS_PER_REV 512
 
-const int stepsPerRevolution = 2048 ;  // change this to fit the number of steps per revolution
-// for your motor
+void setup(){
+pinMode(A,OUTPUT);
+pinMode(B,OUTPUT);
+pinMode(C,OUTPUT);
+pinMode(D,OUTPUT);
+pinMode(E,OUTPUT);
+pinMode(F,OUTPUT);
+pinMode(G,OUTPUT);
+pinMode(H,OUTPUT);
+pinMode(I,OUTPUT);
+pinMode(J,OUTPUT);
+pinMode(K,OUTPUT);
 
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 2, 3, 4, 5);
 
-void setup() {
-  // set the speed at 60 rpm:
-  myStepper.setSpeed(12);
-  // initialize the serial port:
-  Serial.begin(9600);
+digitalWrite(E,1);
+digitalWrite(F,1);
+digitalWrite(G,1);
+digitalWrite(H,1);
+digitalWrite(I,1);
+digitalWrite(J,1);
+digitalWrite(K,1);
 }
 
-void loop() {
-  // step one revolution  in one direction:
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution/4);
-  delay(500);
+void write(int a,int b,int c,int d){
+digitalWrite(A,a);
+digitalWrite(B,b);
+digitalWrite(C,c);
+digitalWrite(D,d);
+}
 
+void onestep(){
+write(1,0,0,0);
+
+write(1,1,0,0);
+
+write(0,1,0,0);
+
+write(0,1,1,0);
+
+write(0,0,1,0);
+
+write(0,0,1,1);
+
+write(0,0,0,1);
+
+write(1,0,0,1);
+
+}
+
+void loop(){
+int i;
+i=0;
+while(i<NUMBER_OF_STEPS_PER_REV){
+onestep();
+i++;
+}
 }
