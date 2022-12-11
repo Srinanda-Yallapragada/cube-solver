5ba0ab9d4d5fdfd9ef990f833681e855fa6b0f8e
krisnalach <55798623+krisnalach@users.noreply.github.com>
2022-12-11 18:54:23 -0500

    Update cube-reader.py

--- cube-reader.py
+++ cube-reader.py
@@ -2,6 +2,9 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+cap.set(3, 768)
+cap.set(4, 768)
+
 sides = ['front','right','left','back','up','down']
 i = 0
 while(i < len(sides)):
@@ -10,11 +13,11 @@ while(i < len(sides)):
     print("Press y to save photo, r to retry.")
     while(True):
         cv2.imshow(sides[i], frame) # display image
-        if cv2.waitKey(1) & 0xFF == ord('r'):
+        if cv2.waitKey(0) & 0xFF == ord('r'):
             print('Retrying.')
             cv2.destroyAllWindows()
             break
-        if cv2.waitKey(1) & 0xFF == ord('y'):
+        if cv2.waitKey(0) & 0xFF == ord('y'):
             cv2.imwrite(f'cube-faces/' + sides[i] + '.png', frame)
             print("Save successful.")
             cv2.destroyAllWindows()
